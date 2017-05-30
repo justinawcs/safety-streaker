36295d17ec3e3ee5bc6c558ce4ee94331f352ff7
Justin Williams <justaddwater843@yahoo.com>
2017-05-30 07:02:38 -0400

    init.sh started, not complete, non functional

--- init.sh
+++ init.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
+# One-Time Setup of Safety-Streaker Boot script, in ~.bashrc
 # Sets up environment for info-beamer, safety-streaker.
-# IMPORTANT Add this script to ~/.bashrc to load at startup.
 #title          : init.sh
 #description    : see above
 #author         : Justin A. Williams
-#date           : 20170527
+#date           : 20170528
 #version        : 0.8
 #usage          : bash init.sh
 #notes          : see below -> ##
 #bash_version   : ???
 #==============================================================================
-## ADD line below (with out ##) to ~/.bashrc
-## sudo ~/safety-streaker/init.sh
+# echo ~
 
-export WORK=/home/pi/info-beamer/
+export WORK=/home/pi/safety-streaker/
 cd $WORK
-alias 00=$WORK/ControlMenu.py
+alias 00='sudo $WORK/ControlMenu.py'
 pidof -x switch.py
 if [[ $? == 0 ]]
   then
