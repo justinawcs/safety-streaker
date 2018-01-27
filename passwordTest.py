ceb83714f7870152e1c15f702fffa24c065a21c4
Justin Williams <justinawcs@gmail.com>
2018-01-27 07:37:11 -0500

    add file read/write to passwordTest.py, work in progress
    still needs password minimums for passcodes

--- passwordTest.py
+++ passwordTest.py
@@ -3,18 +3,40 @@
 #title          : passwordTest.py
 #description    : see above
 #author         : Justin A. Williams
-#date           : 20180124
+#date           : 20180127
 #version        : 0.9
-#usage          : bash passwordTest.py
+#usage          : python passwordTest.py
 #notes          : see below ##
 #bash_version   : 4.3.30(1)-release [bash version]
 #===============================================================================
-## no secuirty password
+## low security password file, in progress needs minimum for passodes
+## next step: hashed password
 import sys
 
-print "Password Tester"
-code = "4021"
+#open and read file
+code_file = open("passcode.data", "r+")
+code = code_file.readline().rstrip()
+
+#passcode reminder
+print "Password Tester. Code is: '" + code + "'"
+
+#get user input
 user_input = raw_input("Enter passcode: ")
 if user_input != code:
 	sys.exit("Wrong Passcode...bye!")
 print "Code accepted."
+
+#change passcode
+user_input =raw_input("Press enter to close, or enter new passcode: ")
+print "Entered code: '" + user_input + "'"
+if user_input == '':
+    sys.exit("Goodbye.")
+
+#write to file
+code = user_input
+code_file.seek(0)
+code_file.write(code)
+code_file.truncate()
+print "Current passcode: '"+ code + "'\n Goodbye."
+code_file.close()
+
