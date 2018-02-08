10aae8adb0f337d5c3defdaeb68e3fc87ec5984a
Justin Williams <justaddwater843@yahoo.com>
2018-02-08 07:57:06 +0000

    Add class functions to emulate bingo.py usage

--- pickleTester.py
+++ pickleTester.py
@@ -15,25 +15,36 @@ class PickleTester:
     name = "Justin"
     age = 32
     fav = ["a", "b", "d"]
-    print "SETUP: "
     about_me = {"name":name, "age":age, "fav":fav}
-    print about_me
-    #print zip["fav"]
-    #about_me = {"Russ", 36}
-    print "CHANGE: "
-    about_me["fav"].append("xyz")
-    print about_me
-    #save
-    pickle.dump( about_me, open( "me.data", "wb" ) )
-    ##print os.popen("cat me.data").read()
-    #load
-    load_me = pickle.load( open( "me.data", "rb" ) )
-    print "OUT: "
-    print load_me
-    name = load_me["name"]
-    age = load_me["age"]
-    fav = load_me["fav"]
-    print (name, age, fav)
-#end PickleTester
+    
+    def __init__(self):
+        print "SETUP: "
+        print self.about_me.values()
+        self.load()
+    
+    def update(self):
+        #print about_me
+        #print zip["fav"]
+        #about_me = {"Russ", 36}
+        print "CHANGE: "
+        self.about_me["fav"].append("xyz")
+        print self.about_me
+        self.save()
+        
+    def save(self):
+        #save
+        pickle.dump( self.about_me, open( "me.data", "wb" ) )
+        ##print os.popen("cat me.data").read()
+    
+    def load(self):
+        #load
+        load_me = pickle.load( open( "me.data", "rb" ) )
+        print "OUT: "
+        print load_me
+        self.name = load_me["name"]
+        self.age = load_me["age"]
+        self.fav = load_me["fav"]
+        print (self.name, self.age, self.fav)
+    #end PickleTester
 
 tt = PickleTester()
