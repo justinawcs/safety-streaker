fdf0db0c3acc9811e2f6f9dcd1878a39e01847b8
Justin Williams <justinawcs@gmail.com>
2018-02-11 06:32:50 -0500

    Start bingo visual, a copy from draft

---
+++ bingo/node.lua
@@ -0,0 +1,81 @@
+gl.setup(1600, 900)
+marginX = 30
+marginY = 20
+
+-- FONTS
+local arial = resource.load_font("Arial.ttf")
+local black = resource.load_font("Arial_Black.ttf")
+local impact = resource.load_font("Impact.ttf")
+local type = resource.load_font("silkscreen.ttf")
+
+-- FILES
+-- local file = resource.load_file(filename)
+local background = resource.load_image("flag.jpg")
+
+--TODO What is purpore of this function?
+function trim(s)
+    return s:match "^%s*(.-)%s*$"
+end
+
+--function checkFiles()
+--watch bestStreak.data file
+util.file_watch("bestStreak.data", function(content)
+      streak = trim(content)
+  end)
+--watch lastInjury.data file and calc timeSince
+util.file_watch("lastInjury.data", function(content)
+      injuryFile = trim(content)
+      position = string.find(injuryFile, "\n")
+      injurySec = injuryFile:sub(1, position - 1)
+  end)
+--end
+
+function align_right(font, str, size)
+    -- aligns text on right of screen with given marginX
+    wide = font:width(str, size)
+    return WIDTH - marginX - wide
+end
+
+function align_center(font, str, size)
+    -- aligns text on the center of screen with given marginX
+    wide = font:width(str, size)
+    return ( WIDTH - wide ) / 2
+end
+
+--create vertical list of numbers from start, using fonts, sizing, spacing
+function column(font, startNum, size, spacing)
+    topMargin = 60
+    high = font:height(startNum, size)
+end
+
+function grid()
+    --IDEA this layout should be sliced from image program
+end
+
+function node.render()
+    --checkFiles()
+    --gl.clear(.2, .37, 0, 1) -- set background sage green
+    background:draw(0, 0, WIDTH, HEIGHT, .6)
+    timeNow = math.floor(os.time())
+    secondsSince = timeNow - injurySec
+    daysSince = math.floor((timeNow - injurySec) / 86400 )
+    hoursSince = math.floor(((timeNow - injurySec) % 86400) / 3600)
+    -- type:write(0, 0, injuryFile.." "..timeSince, 20, 1, 1, 1, 1)
+    -- font:write(XPOS, YPOS, "TEXT", SCALE, R,G,B,Alpha)
+    black:write(24, 54, "SAFETY STARTS WITH YOU!", 100, 0,0,0,.4)
+    black:write(20, 50, "SAFETY STARTS WITH YOU!", 100, 1,1,1,1)
+    linex = align_center(arial, "This Property has enjoyed", 80)
+    arial:write(linex, 160, "This Property has enjoyed", 80, .7,.75,.7,1)
+    black:write(32, 252, daysSince.." Days, "..hoursSince.." Hours", 150, 1,0,0,1)
+    black:write(30, 250, daysSince.." Days, "..hoursSince.." Hours", 150, .3,0,0,1)
+    arial:write(600, 400, "without a lost time accident.", 80, .7,.75,.7,1)
+    arial:write(25, 500, "The best previous record was", 80, .7,.75,.7,1)
+    --black:write(618, 598, "123 DAYS", 180, 1,0,0,1)
+    xx = align_right(black, streak.." days.", 180)
+    black:write(xx+4, 604, streak.." days.", 180, 1,0,0,1)
+    black:write(xx, 600, streak.." days.", 180, .3,0,0,.95)
+    impact:write(320, 800, "STAY SAFE. THINK SAFETY!", 100, 1,1,1,1)
+
+    --resource.render_child(""):draw(50, 200, 300, 400)
+
+end
