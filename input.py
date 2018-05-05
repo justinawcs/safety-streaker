6f7ce7cc729f2f55c78f885275d4d1b18de95264
Justin Williams <justaddwater843@yahoo.com>
2018-05-05 13:51:32 +0000

    Move to input.py, began implementation

--- input.py
+++ input.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python
-# Test for new input implementation.
-#title          : inputTest.py
+# Takes and verifies type of general user inputs.
+#title          : input.py
 #description    : see above
 #author         : Justin A. Williams
-#date           : 20180411 [year-month-day]
+#date           : 20180505 [year-month-day]
 #version        : 0.9
-#usage          : python inputTest.py
+#usage          : python input.py
 #notes          : Goal: return only integer, float, string 
 #bash_version   : 4.3.30(1)-release [bash version]
 #===============================================================================
 import os
-# print int("7")
-# print int("seven")
-# print float("6")
-# print str(5+5)
 
 def takeInput(prompt, expectedType):
     if expectedType == int or expectedType == float:
@@ -37,9 +33,9 @@ def takeInput(prompt, expectedType):
         print expectedType,  "was expected, not: ",  type(sel) 
         return None
 
-def takeDate(promptType):
+def takeDate(promptMsg):
     #take date
-    print "%s in MM/DD/YYYY then press [ENTER]" % promptType
+    print "%s using MM/DD/YYYY then press [ENTER]" % promptMsg
     print "example: 02/14/2001"
     day  = str(takeInput("Enter Date", basestring))
     #take time
@@ -52,18 +48,18 @@ def takeDate(promptType):
     cmd1 = "date -d '" + day +" "+ time + "'"
     cmd2 = "date -d '" + day +" "+ time + "' +%s"
     cmdCheck = str(cmd1 + "; echo $?")
-    result = str(os.popen(cmd1).read()).rstrip()
+    result1 = str(os.popen(cmd1).read()).rstrip()
+    result2 = str(os.popen(cmd2).read()).rstrip()
     result_err = str(os.popen(cmdCheck).read()).rstrip()
     get_err = result_err.splitlines()
     error_code = get_err[-1]
     #print(error_code, type(error_code))
     if(error_code == "0"): #Zero is success
-        return "Date Accecpted: "+ result
+        print "Date Accecpted: "+ result1+"  "+result2
+        return result1, result2
     else:
-        return "Date Rejected: "+ result_err
-
-def takeBinary(prompt):
-    None
+        print "Date Rejected: "+ result_err
+        return None
     
 def testInput():
     #print takeInput("Int?", int)
