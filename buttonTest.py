07d95c770f4982f41123f5c19dfb1f9739246063
Justin Williams <justaddwater843@yahoo.com>
2018-07-18 06:42:37 +0000

    Add timeout to buttonTest.py; needs live testing

--- buttonTest.py
+++ buttonTest.py
@@ -20,12 +20,20 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 print "[switch.py]  Waiting for 'Injury Now' Button to be pressed..."
+print "Note: This script will close automatically 10 seconds \n" + \
+        "after last button press." 
 waiting = True
+count_down = 10
+sleep_time = .1
 while waiting:
     input_state = GPIO.input(23)
-    time.sleep(.1)
     if input_state == False:
         print('Injury Now Button Pressed!')
+        count_down = 10
         #print os.popen("/home/pi/safety-streaker/injuryNow.sh").read()
         waiting = False
-	time.sleep(1)
+    if count_down < 0:
+        print("Input timed out. Goodbye.")
+        waiting = False
+    count_down -= sleep_time
+    time.sleep(sleep_time)
