80fa1473e4d05df85853f15cfce557c28eea5a89
Justin Williams <justinawcs@gmail.com>
2018-09-06 19:38:18 -0400

    Add the flag-seconds visual
    Shows the flag visual with minutes and seconds instead of days and hours.
    A good example and can be used to live test Injury Now button.

---
+++ flag-seconds/node.lua
@@ -0,0 +1,138 @@
+gl.setup(1600, 900)
+marginX = 30
+marginY = 20
+
+-- FONTS
+local arial = resource.load_font("Arial.ttf")
+local black = resource.load_font("Arial_Black.ttf")
+local impact = resource.load_font("Impact.ttf")
+local typer = resource.load_font("silkscreen.ttf")
+local shadow = resource.create_colored_texture(0,0,0,1)
+
+-- FILES
+-- local file = resource.load_file(filename)
+local background = resource.load_image("flag.jpg")
+local usa = resource.load_video{
+    file = "usa.mp4";
+    looped = true;}
+
+function trim(s)
+    return s:match "^%s*(.-)%s*$"
+end
+
+--function checkFiles()
+--watch bestStreak.data file
+util.file_watch("bestStreak.data", function(content)
+      streak = trim(content)
+  end)
+--watch lastInjury.data file and calc timeSince
+util.file_watch("lastInjury.data", function(content)
+      injuryFile = trim(content)
+      position = string.find(injuryFile, "\n")
+      injurySec = injuryFile:sub(1, position - 1)
+  end)
+--end
+
+function align_right(font, str, size)
+    -- aligns text on right of screen with given marginX
+    wide = font:width(str, size)
+    return WIDTH - marginX - wide
+end
+
+function align_center(font, str, size)
+    -- aligns text on the center of screen with given marginX
+    wide = font:width(str, size)
+    return ( WIDTH - wide ) / 2
+end
+
+function banner(tall, daysSinceDecimal)
+    start_bottom = HEIGHT - tall
+    text_height = tall * 0.85
+    text_padding = (tall * 0.15)
+    start_y = start_bottom + text_padding
+    split_lines = text_height / 2.3
+    --shadow box
+    shadow:draw(0, start_bottom, WIDTH, start_bottom+tall, .80)
+    --days
+    black:write(marginX, start_y, daysSince, text_height,.8,.8,.8,1 )
+    nextX = black:width(daysSince, text_height) + 15+ marginX
+    daysVertical(nextX, start_bottom, black, tall, 0.85, 1, 1, 1, 1)
+    nextX = black:width("_", text_height/4) + 30 + nextX
+    --hours
+    black:write(nextX, start_y, hoursSince, text_height,.8,.8,.8,1 )
+    nextX = black:width(hoursSince, text_height) + 15 +nextX
+    hoursVertical(nextX, start_bottom, black, tall, 0.85, 1, 1, 1, 1)
+    nextX = black:width("_", text_height/5) + 35 + nextX
+    --since text
+    dayStr = "Since Last Accident"
+    arial:write(nextX, start_y, dayStr, text_height*.6,.8,.8,.8,1 )
+    xx = align_right(black, streak.." Days", text_height*.6)
+    nextX = arial:width(dayStr, text_height*.6) + 30 + nextX
+    --start other side: days
+    nextR = WIDTH - marginX
+    rr = black:width("_", text_height/4)
+    daysVertical(nextR-rr, start_bottom, black, tall, 0.85, 1, 1, 1, 1)
+    nextR = nextR - rr - black:width(streak, text_height) - 20
+    black:write(nextR, start_y, streak, text_height,.8,.8,.8,1 )
+    rr = black:width(streak, text_height)
+    --best record text
+    bestStr = "Best Record"
+    nextR = nextR - arial:width(bestStr, text_height*.6) - 20
+    floor_y = HEIGHT - (text_height *.6)
+    arial:write(nextR, floor_y, bestStr,text_height*.6,.8,.8,.8,1 )
+end
+
+function daysVertical(x_pos, y_pos, font, text_height, correction, rdV, grV, blV, alphaV)
+    --vertical stack of days letters
+    text_scaled = (text_height/4) / correction
+    spacing = (text_height/4 * correction)
+    font:write(x_pos, y_pos+(spacing*0)/correction, "D", text_scaled, rdV,grV,blV,alphaV)
+    font:write(x_pos, y_pos+(spacing*1)/correction, "A", text_scaled, rdV,grV,blV,alphaV)
+    font:write(x_pos, y_pos+(spacing*2)/correction, "Y", text_scaled, rdV,grV,blV,alphaV)
+    font:write(x_pos, y_pos+(spacing*3)/correction, "S", text_scaled, rdV,grV,blV,alphaV)
+end
+
+function hoursVertical(x_pos, y_pos, font, text_height, correction, rdV, grV, blV, alphaV)
+    --vertical stack of hourss letters
+    text_scaled = text_height / (5 * correction)
+    spacing = (text_height/5) * correction
+    font:write(x_pos, y_pos+(spacing*0)/correction, "H", text_scaled, rdV,grV,blV,alphaV)
+    font:write(x_pos, y_pos+(spacing*1)/correction, "O", text_scaled, rdV,grV,blV,alphaV)
+    font:write(x_pos, y_pos+(spacing*2)/correction, "U", text_scaled, rdV,grV,blV,alphaV)
+    font:write(x_pos, y_pos+(spacing*3)/correction, "R", text_scaled, rdV,grV,blV,alphaV)
+    font:write(x_pos, y_pos+(spacing*4)/correction, "S", text_scaled, rdV,grV,blV,alphaV)
+
+end
+function round(num, numDecimalPlaces)
+  return tonumber(string.format("%." .. (numDecimalPlaces or 0) .. "f", num))
+end
+
+function node.render()
+    --checkFiles()
+    --gl.clear(.2, .37, 0, 1) -- set background sage green
+    --background:draw(0, 0, WIDTH, HEIGHT, .6)
+    usa:draw(0, 0, WIDTH, HEIGHT, .6)
+    timeNow = math.floor(os.time())
+    secondsSince = timeNow - injurySec
+    minutesSince = math.floor((timeNow - injurySec) / 60 )
+    secsSince = math.floor(((timeNow - injurySec) % 60) / 1)
+    daysSinceDecimal = round( ((timeNow - injurySec ) / 86400), 2)
+    -- type:write(0, 0, injuryFile.." "..timeSince, 20, 1, 1, 1, 1)
+    -- font:write(XPOS, YPOS, "TEXT", SCALE, R,G,B,Alpha)
+    black:write(24, 54, "SAFETY STARTS WITH YOU!", 100, 0,0,0,.4)
+    black:write(20, 50, "SAFETY STARTS WITH YOU!", 100, 1,1,1,1)
+    arial:write(25, 160, "The Marriott Evergreen Family has enjoyed", 80, .7,.7,.7,1)
+    black:write(32, 252, minutesSince.." Min, "..secsSince.." Sec", 150, 1,0,0,1)
+    black:write(30, 250, minutesSince.." Min, "..secsSince.." Sec", 150, .3,0,0,1)
+    arial:write(600, 400, "without a lost time accident.", 80, .7,.7,.7,1)
+    arial:write(25, 500, "The best previous record was:", 80, .7,.7,.7,1)
+    --black:write(618, 598, "123 DAYS", 180, 1,0,0,1)
+    xx = align_right(black, streak.." days", 180)
+    black:write(xx+4, 604, streak.." Days", 180, 1,0,0,.6)
+    black:write(xx, 600, streak.." Days", 180, .3,0,0,.95)
+    impact:write(324, 804, "STAY SAFE. THINK SAFETY!", 100, 0,0,0,.6)
+    impact:write(320, 800, "STAY SAFE. THINK SAFETY!", 100, 1,1,1,1)
+    --banner(120, daysSinceDecimal)
+    --resource.render_child("HI"):draw(50, 200, 300, 400)
+
+end
